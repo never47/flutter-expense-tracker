52d613bb8e91754cb215deb746076d9dc9ef329a
never47 <guseinovi2002@gmail.com>
2024-03-07 13:58:36 +0100

    initial commit

---
+++ lib/widgets/expenses.dart
@@ -0,0 +1,58 @@
+import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
+import 'package:flutter/material.dart';
+import 'package:expense_tracker/models/expense.dart';
+import 'package:flutter/widgets.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() {
+    return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: 'Flutter Course',
+      amount: 19.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: 'AVATAR cinema',
+      amount: 9.99,
+      date: DateTime.now(),
+      category: Category.leisure,
+    ),
+  ];
+
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(
+      context: context,
+      builder: (ctx) => Text('Modal bottom sheet'),
+    );
+  }
+
+  @override
+  Widget build(dynamic context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Expense Tracker'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: _openAddExpenseOverlay,
+          ),
+        ],
+      ),
+      body: Column(children: [
+        const Text('chart'),
+        Expanded(
+          child: ExpensesList(expenses: _registeredExpenses),
+        ),
+      ]),
+    );
+  }
+}
