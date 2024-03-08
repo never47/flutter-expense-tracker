54a85c8e889a3a7e12c3519384a2d2fa0a369c8e
never47 <guseinovi2002@gmail.com>
2024-03-08 12:04:03 +0100

    fix: closing current snackbar if next one openes

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -45,13 +45,43 @@ class _ExpensesState extends State<Expenses> {
   }
 
   void _removeExpense(Expense expense) {
+    final expenseIndex = _registeredExpenses.indexOf(expense);
+
     setState(() {
       _registeredExpenses.remove(expense);
     });
+
+    ScaffoldMessenger.of(context).clearSnackBars();
+
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration: const Duration(seconds: 3),
+        content: const Text('Expense deleted'),
+        action: SnackBarAction(
+          label: 'Undo',
+          onPressed: () {
+            setState(() {
+              _registeredExpenses.insert(expenseIndex, expense);
+            });
+          },
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(dynamic context) {
+    Widget mainContent = const Center(
+      child: Text('No expenses found. Start adding some.'),
+    );
+
+    if (_registeredExpenses.isNotEmpty) {
+      mainContent = ExpensesList(
+        expenses: _registeredExpenses,
+        onRemoveExpense: _removeExpense,
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Expense Tracker'),
@@ -64,12 +94,7 @@ class _ExpensesState extends State<Expenses> {
       ),
       body: Column(children: [
         const Text('chart'),
-        Expanded(
-          child: ExpensesList(
-            expenses: _registeredExpenses,
-            onRemoveExpense: _removeExpense,
-          ),
-        ),
+        Expanded(child: mainContent),
       ]),
     );
   }
